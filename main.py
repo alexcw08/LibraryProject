1a8dc319e83b0fc02beb783586b84d6e8bd614b8
Alex C <calex781@gmail.com>
2023-07-14 11:58:52 -0400

    Create a Library class with methods to print a summary and add book, movie, and video game.

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import inquirer
 
+class Library:
+    def __init__(self) -> None:
+        self.books = {}
+        self.movies = {}
+        self.videoGames = {}
+
+    def getSummary(self):
+        """ Prints a summary of the librarys current holdings. """
+        print('')
+        print(f'My Collection has:')
+        print(f'{len(self.books)} Books \n{len(self.movies)} Movies \n{len(self.videoGames)} Video Games \n')
+        print('')
+        
+    def addBook(self, book):
+        """ Takes in a book and adds it to the librarys dictionary. """
+        self.books[book.name] = book
+        print(f'[Success] Book successfully added.')
+
+    def addMovie(self, movie):
+        """ Takes in a movie and adds it to the librarys dictionary. """
+        self.movies[movie.name] = movie
+        print(f'[Success] Movie successfully added.')
+
+    def addVideoGame(self, videoGame):
+        """ Takes in a video game and adds it to the librarys dictionary. """
+        self.videoGames[videoGame.name] = videoGame
+        print(f'[Success] Video Game successfully added.')
+
 if __name__ == "__main__":
     # Ask user what they want to do
     userChoices = [inquirer.List('choice', message='What would you like to do?', choices=['[Form]Add a book', '[Form]Add a movie', '[Form]Add a video game', 'Quit'])]
