972d7c13b7e3553f743a4dc4043dd180a91dbe35
Alex C <calex781@gmail.com>
2023-07-14 11:53:43 -0400

    Add array to hold prompt for users choice. Add while loop for main program and conditions based on users choice.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import inquirer
 
 if __name__ == "__main__":
-    pass
+    # Ask user what they want to do
+    userChoices = [inquirer.List('choice', message='What would you like to do?', choices=['[Form]Add a book', '[Form]Add a movie', '[Form]Add a video game', 'Quit'])]
+
+    while True:
+        userRes = inquirer.prompt(userChoices)
+        if userRes['choice'] == '[Form]Add a book':
+            print('You chose to add a book')
+        elif userRes['choice'] == '[Form]Add a movie':
+            print('You chose to add a movie')
+        elif userRes['choice'] == '[Form]Add a video game':
+            print('You chose to add a video game')
+        else:
+            break
